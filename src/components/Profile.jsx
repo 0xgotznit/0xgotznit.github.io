91be51c5e346c2ed5c260256a8b00be15d09cc5d
0xgotznit <ayoubidel64@gmail.com>
2024-01-30 20:26:20 +0100

    :sparkles: add translations for profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,49 +1,46 @@
-// Profile.js
 import React from 'react';
-import { Box, Text, Heading, VStack, Center, UnorderedList, ListItem } from '@chakra-ui/react';
+import {Box, Center, Heading, ListItem, Text, UnorderedList, VStack} from '@chakra-ui/react';
+import {useTranslation} from "react-i18next";
 
 const Profile = () => {
 
+    const {t} = useTranslation();
 
-  return (
-    <Box mt={2} p={6} borderWidth={1} borderRadius="lg" shadow="lg" bg="black" color="white" maxW={['99%', '99%', '700px']} mx="auto">
-      <Heading as="h2" fontSize="2xl" mb={4}>
-        About Me
-      </Heading>
-      <Text fontSize="lg" mb={4}>
-       
-        <br />
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor, turpis nec
-        fringilla hendrerit, felis nisi venenatis quam, a interdum velit metus quis justo.
-        Integer eget lectus id enim auctor congue.
-        <br />
-        
-        <br />
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor, turpis nec
-        fringilla hendrerit, felis nisi venenatis quam, a interdum velit metus quis justo.
-        Integer eget lectus id enim auctor congue.
-      </Text>
-      <VStack alignItems="start" spacing={2}>
-        <Heading as="h3" fontSize="lg">
-          <span role="img" aria-label="English Flag" style={{ marginRight: '8px' }}>
-            🇬🇧
-          </span>
-          What I Did (English):
-        </Heading>
-        <UnorderedList>
-          <ListItem>Designed user-friendly websites</ListItem>
-          <ListItem>Developed responsive web applications</ListItem>
-          <ListItem>Collaborated with cross-functional teams</ListItem>
-          <ListItem>Implemented SEO best practices</ListItem>
-        </UnorderedList>
-      </VStack>
-      <Center mt={4}>
-        <Text fontSize="lg" fontWeight="bold" color="teal.500">
-          Learn more about me
-        </Text>
-      </Center>
-    </Box>
-  );
+
+    return (
+        <Box mt={2} p={6} borderWidth={1} borderRadius="lg" shadow="lg" bg="black" color="white"
+             maxW={['99%', '99%', '90%', '85%', '80%']} mx="auto">
+            <Heading as="h2" fontSize="2xl" mb={4}>
+                {t('software_engineer')}
+            </Heading>
+            <Text fontSize="md" mb={4} fontFamily="Roboto, sans-serif" lineHeight="tall">
+                <strong>{t('who_loves_go_rust')}</strong> {t('passion_memory_management')}
+            </Text>
+            <Text fontSize="md" mb={4} fontFamily="Roboto, sans-serif" lineHeight="tall">
+                {t('more_about_me')}
+            </Text>
+            <Text fontSize="md" mb={4} fontFamily="Roboto, sans-serif" lineHeight="tall">
+                {t('open_source_playground')}
+            </Text>
+            <VStack alignItems="start" spacing={2}>
+                <Heading as="h3" fontSize="lg">
+                    {t('fun_bytes')}
+                </Heading>
+                <UnorderedList>
+                    <ListItem>{t('list_memory_management')}</ListItem>
+                    <ListItem>{t('list_blockchain_believer')}</ListItem>
+                    <ListItem>{t('list_fluent_go_rust')}</ListItem>
+                    <ListItem>{t('list_graduated_software_engineering')}</ListItem>
+                    <ListItem>{t('list_anime_enthusiast')}</ListItem>
+                </UnorderedList>
+            </VStack>
+            <Center mt={4}>
+                <Text fontSize="lg" fontWeight="bold" color="teal.500">
+                    {t('dive_deeper')}
+                </Text>
+            </Center>
+        </Box>
+    );
 }
 
 export default Profile;
--- src/i18n.js
+++ src/i18n.js
@@ -1,45 +1,72 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 // Translations
 const resources = {
     en: {
         translation: {
-          'hi':'Hi 👋🏻, I\'m',
-          'contact': 'Contact',
-          'cv': 'CV',
-          'blog': 'Blog',
-          'code': 'Code',
-          'watchlist': 'Watchlist',
-          'links': 'Links',
-          // ...more translations for English
-        }
-      },
-      fr: {
-        translation: {
-          'hi':'Salut 👋🏻, c\'est',
-          'contact': 'Contact',
-          'cv': 'CV',
-          'blog': 'Blog',
-          'code': 'Code',
-          'watchlist': 'Liste de suivi',
-          'links': 'Liens',
+            'hi': 'Hi 👋🏻, I\'m',
+            'contact': 'Contact',
+            'cv': 'CV',
+            'blog': 'Blog',
+            'code': 'Code',
+            'watchlist': 'Watchlist',
+            'roadmap': 'Roadmap',
+
+            'software_engineer': 'Software Engineer',
+            'who_loves_go_rust': 'Who\'s equally in love with Go and Rust.',
+            'passion_memory_management': 'My passion lies in unraveling the mysteries of memory management - a big shoutout to Rust for that! - and diving into the exhilarating world of multithreading and multiprocessing. Thanks to Go, it\'s like having superpowers but in the coding world!',
+            'more_about_me': 'But wait, there\'s more! I\'m also an ardent explorer of blockchain technology, constantly amazed by its endless possibilities.',
+            'open_source_playground': 'Open-source is my playground – if I spot a bug, I\'ll either fix it or make sure to bring it to attention.',
+            'fun_bytes': 'Fun Bytes',
+            'list_memory_management': 'Memory management enthusiast - because who doesn\'t love a well-organized brain?',
+            'list_blockchain_believer': 'Blockchain believer: on a quest to uncover its secrets.',
+            'list_fluent_go_rust': 'Fluent in both Go and Rust: like being bilingual, but cooler.',
+            'list_graduated_software_engineering': 'Graduated in Software Engineering with a focus on Parallelism and Concurrency in distributed systems.',
+            'list_anime_enthusiast': 'Anime Enthusiast - One Piece isn\'t just a show, it\'s a lifestyle!',
+            'dive_deeper': 'Dive Deeper Into My World',
 
         }
-      },
-      // Add more languages here
+    },
+    fr: {
+        translation: {
+            'hi': 'Salut 👋🏻, c\'est',
+            'contact': 'Contact',
+            'cv': 'CV',
+            'blog': 'Blog',
+            'code': 'Code',
+            'watchlist': 'Veille',
+            'roadmap': 'Plan',
+
+
+            'software_engineer': 'Ingénieur logiciel',
+            'who_loves_go_rust': 'Qui aime autant Go que Rust.',
+            'passion_memory_management': 'Ma passion réside dans le démêlement des mystères de la gestion de la mémoire - un grand merci à Rust pour cela ! - et plonger dans le monde exaltant du multithreading et du multiprocessing. Grâce à Go, c\'est comme avoir des super-pouvoirs mais dans le monde du codage !',
+            'more_about_me': 'Mais attendez, il y a plus ! Je suis aussi un explorateur passionné de la technologie blockchain, constamment émerveillé par ses possibilités infinies.',
+            'open_source_playground': 'L’open-source est mon terrain de jeu – si je repère un bug, je le corrige ou m’assure de le signaler.',
+            'fun_bytes': 'Bouchées de plaisir',
+            'list_memory_management': 'Passionné de gestion de la mémoire - parce que qui n\'aime pas un cerveau bien organisé ?',
+            'list_blockchain_believer': 'Croyant de la blockchain : en quête de découvrir ses secrets.',
+            'list_fluent_go_rust': 'Parlant couramment Go et Rust : comme être bilingue, mais plus cool.',
+            'list_graduated_software_engineering': 'Diplômé en génie logiciel avec une spécialisation en parallélisme et concurrence dans les systèmes distribués.',
+            'list_anime_enthusiast': 'Enthousiaste d\'anime - One Piece n\'est pas seulement une émission, c\'est un mode de vie !',
+            'dive_deeper': 'Plongez plus profondément dans mon monde',
+
+        }
+    },
+
 };
 
 i18n
-  .use(initReactI18next)
-  .use(LanguageDetector)
-  .init({
-    resources,
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false
-    }
-  });
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .init({
+        resources,
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false
+        }
+    });
 
 export default i18n;
